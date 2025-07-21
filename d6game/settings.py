0f55cc8c7c803aeff704166c0ef96a02c51a6ccd
Serge1998 <serge.ntimena@fse-uy1.cm>
2025-07-21 01:45:47 +0100

    Ajout d6game6.onrender.com aux ALLOWED_HOSTS

--- d6game/settings.py
+++ d6game/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i)k(&@+=c)l4l6_+_sns*)e#ohg991947izey5(eld%g)u1#4f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['d6game6.onrender.com']
 
 
 # Application definition
